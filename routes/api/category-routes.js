6a9e62693440861d1092f10f2ae567c4360fcfb5
Carlos Febres <febres0492@gmail.com>
2024-06-24 18:11:59 -0400

    Testing category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,25 +4,30 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+    // find all categories
+    // be sure to include its associated Products
+    res.json("get all categories")
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+    // find one category by its `id` value
+    // be sure to include its associated Products
+    res.json("get one category by id")
 });
 
 router.post('/', (req, res) => {
-  // create a new category
+    // create a new category
+    res.json("create a new category")
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+    // update a category by its `id` value
+    res.json("update a category by id")
 });
 
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+    // delete a category by its `id` value
+    res.json("delete a category by id")
 });
 
 module.exports = router;
